18ef84c0f3cc817686d9b8d1010ca827f6e4f8c0
Titouan Aclr <122918576+titouan-aclr@users.noreply.github.com>
2024-09-30 12:10:20 +0200

    init routes

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { GameListComponent } from './components/game-list/game-list.component';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, GameListComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
